17a175ef277b0771ef560ed0968f210110d6a19d
HRojotiana <hei.rojotiana@gmail.com>
2023-12-14 19:45:48 +0300

    fix: added annotation before PrimeController

--- src/main/java/com/hei/rojo/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/hei/rojo/endpoint/rest/controller/PrimeController.java
@@ -3,8 +3,14 @@ package com.hei.rojo.endpoint.rest.controller;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
+import com.hei.rojo.PojaGenerated;
+import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@PojaGenerated
+@RestController
+@Value
 public class PrimeController {
     @GetMapping("/new-prime")
     public BigInteger isPrime(){
