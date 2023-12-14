255122bca571a8b5608b2be94a4ead42d30b9977
HRojotiana <hei.rojotiana@gmail.com>
2023-12-14 16:36:43 +0300

    initial commit

---
+++ src/main/java/com/hei/rojo/repository/DummyRepository.java
@@ -0,0 +1,15 @@
+package com.hei.rojo.repository;
+
+import com.hei.rojo.PojaGenerated;
+import com.hei.rojo.repository.model.Dummy;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@PojaGenerated
+@Repository
+public interface DummyRepository extends JpaRepository<Dummy, String> {
+
+  @Override
+  List<Dummy> findAll();
+}
---
+++ src/main/java/com/hei/rojo/repository/DummyUuidRepository.java
@@ -0,0 +1,15 @@
+package com.hei.rojo.repository;
+
+import com.hei.rojo.PojaGenerated;
+import com.hei.rojo.repository.model.DummyUuid;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@PojaGenerated
+@Repository
+public interface DummyUuidRepository extends JpaRepository<DummyUuid, String> {
+
+  @Override
+  Optional<DummyUuid> findById(String id);
+}
