36681fac61ae8bfaf8bd5e18053425def95a65e8
HRojotiana <hei.rojotiana@gmail.com>
2023-12-14 19:40:34 +0300

    fix: update Random to SecureRandom

--- src/main/java/com/hei/rojo/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/hei/rojo/endpoint/rest/controller/PrimeController.java
@@ -1,14 +1,14 @@
 package com.hei.rojo.endpoint.rest.controller;
 
 import java.math.BigInteger;
-import java.util.Random;
+import java.security.SecureRandom;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class PrimeController {
     @GetMapping("/new-prime")
     public BigInteger isPrime(){
-        Random random = new Random();
-        return BigInteger.probablePrime(10_000, random);
+        SecureRandom secureRandom = new SecureRandom();
+        return BigInteger.probablePrime(10_000, secureRandom);
     }
 }
