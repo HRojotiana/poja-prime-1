7d1b26b9f26259b47903c3d5c9b786aa54da2953
HRojotiana <hei.rojotiana@gmail.com>
2023-12-14 19:17:01 +0300

    feat: add test for PrimeController

---
+++ src/test/java/com/hei/rojo/endpoint/rest/controller/PrimeControllerIT.java
@@ -0,0 +1,20 @@
+package com.hei.rojo.endpoint.rest.controller;
+
+import com.hei.rojo.conf.FacadeIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PrimeControllerIT extends FacadeIT {
+    @Autowired PrimeController primeController;
+
+    @Test
+    void isPrimeTest() {
+        BigInteger primeNumber = primeController.isPrime();
+        assertTrue(primeNumber.isProbablePrime(99), "Number is not a prime number");
+    }
+}
